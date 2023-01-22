503f1cc6fcdc5da9ace31d3073f449c6206cd280
Feterson1 <Feterson@bk.ru>
2023-01-22 13:50:09 +0600

    search_input

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
 
 
   const [articles,setArticles] = useState([]);
+  const [searchWord,setSearchWord] = useState('');
 
 
   useEffect(()=>{
@@ -32,14 +33,31 @@ function App() {
     setArticles(data.data.response.results);
   }
 
+  const inputHandler = (e) => {
+    setSearchWord(e.target.value);
+  }
+  const buttonHandler = (e) =>{
+    getData(searchWord);
+   
+  }
+
   
  
 
   return (
     <div className="App">
+      <input type="text" value={searchWord} onChange={inputHandler}/>
+      <button onClick={buttonHandler}>Search</button>
       {articles.map(article => {
         return (
-          <p key={article.id}><a href={article.webUrl}>{article.webTitle}</a></p>
+          <p key={article.id}>
+            <a href={article.webUrl}>
+              {article.webTitle.split(searchWord)[0]}
+            <span style={{backgroundColor: 'Yellow'}}>{searchWord}</span>
+            {article.webTitle.split(searchWord)[1]}
+          </a>
+          </p>
+
         )
       })}
      
