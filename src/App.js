c6ea1db96efe8c1c7341c3dc2f1deac5e33751fb
Feterson1 <Feterson@bk.ru>
2023-01-23 19:24:45 +0600

    home page layout

--- src/App.js
+++ src/App.js
@@ -15,22 +15,29 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <div>
-          <NavLink exact to ='/'>Main</NavLink>
-          <NavLink to ='/politics'>Politics</NavLink>
-          <NavLink to ='/sport'>Sport</NavLink>
-          <NavLink to ='/media'>Media</NavLink>
-          <NavLink to ='/economy'>Economy</NavLink>
+        <div className='Main-Banner'>
+          <div className='Nav-Link-Bar'>
+            <NavLink exact to='/' className='Main-Banner-Link'>Main</NavLink>
+            <NavLink to='/politics' className='Main-Banner-Link'>Politics</NavLink>
+            <NavLink to='/sport' className='Main-Banner-Link'>Sport</NavLink>
+            <NavLink to='/media' className='Main-Banner-Link'>Media</NavLink>
+            <NavLink to='/economy' className='Main-Banner-Link'>Economy</NavLink>
+          </div>
+          <div>
+          <h1>Feterson</h1>
+          <p>The London Ambulance Service (LAS) has said that a computer crash on 8 June forced staff to revert to pen and paper to record calls from the public.
+            The problem arose because of technical issues implementing a new emergency computer system.</p>
+            </div>
         </div>
         <Routes>
-          <Route exact path='/' element={<MainPage/>}/>
-          <Route path='/economy' element={<Economy/>}/>
-          <Route path='/politics' element={<Politics/>}/>
-          <Route path='/sport' element={<Sport/>}/>
-          <Route path='/media' element={<Media/>}/>
+          <Route exact path='/' element={<MainPage />} />
+          <Route path='/economy' element={<Economy />} />
+          <Route path='/politics' element={<Politics />} />
+          <Route path='/sport' element={<Sport />} />
+          <Route path='/media' element={<Media />} />
         </Routes>
 
-        
+
       </div>
     </BrowserRouter>
   );
--- src/components/NewsInfo/NewsInfo.jsx
+++ src/components/NewsInfo/NewsInfo.jsx
@@ -5,14 +5,14 @@ import React from "react";
 const NewsInfo = ({article}) => {
 
     return (
-        <div key={article.id}>
+        <div key={article.id} className = "News-Info">
             <h2><a href={article.webUrl}>
                 {article.webTitle}
             </a>
             </h2>
             <img src={newsImage} style = {{width:'500px',height:'500px'}} alt="" />
             <p>{article.webPublicationDate.split('T')[0]}</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, minus, reprehenderit voluptas vel adipisci beatae modi architecto quae aspernatur consectetur quo atque ea quos deserunt est illo. Totam, voluptatum cumque.</p>
+            <p id='Date-Info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, minus, reprehenderit voluptas vel adipisci beatae modi architecto quae aspernatur consectetur quo atque ea quos deserunt est illo. Totam, voluptatum cumque.</p>
         </div>
     )
 
